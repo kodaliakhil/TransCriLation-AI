965afc3838b36db8f96cf9d18f1db93a58fe1dad
Akhil <kodaliakhil1999@gmail.com>
2025-03-24 12:54:16 +0530

    workflow dispactch added and yml file name change

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,9 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base:'/transcrilation/'
+  base: "/transcrilation/",
+  server: {
+    open: true,
+    port: 3001,
+  },
 });
